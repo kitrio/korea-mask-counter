09d7dd6b6f20361682f8204a34ac3c0d83886e42
kit rio <mario64aq@gmail.com>
2020-03-11 20:23:12 +0900

    init

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,9 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
+  globals: {
+    options: false
+  },
   overrides: [
     {
       files: [
