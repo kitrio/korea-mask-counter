4faa1e0a106a302d386d1fc2a6f60a1203513c3a
kit rio <mario64aq@gmail.com>
2020-03-12 04:02:58 +0900

    Add leaflet library

--- src/main.js
+++ src/main.js
@@ -3,11 +3,13 @@ import './plugins/axios'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import vuetify from './plugins/vuetify'
+import leaflet from 'leaflet'
 Vue.config.productionTip = false
-
+Vue.prototype.$lealet = leaflet
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
